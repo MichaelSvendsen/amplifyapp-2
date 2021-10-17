440ef62f29a2cc379d998a74fbf62ba7a6d1aad2
Michael Svendsen <msvendsen.private@gmail.com>
2021-10-18 01:21:01 +0200

    Added storage and image support

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -28,7 +28,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://h2ukeo3z7vextlrzztfwb43jry.appsync-api.us-east-2.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-ydbherhz6naj3j2cdevadyzo4q"
+    "aws_appsync_apiKey": "da2-ydbherhz6naj3j2cdevadyzo4q",
+    "aws_user_files_s3_bucket": "amplify2171039-staging",
+    "aws_user_files_s3_bucket_region": "us-east-2"
 };
 
 
